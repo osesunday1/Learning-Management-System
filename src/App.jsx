9237b59d6b1e5ec6915df2c9caced883dcfc0de7
osesunday1 <ose.sunday@gmail.com>
2025-02-02 15:02:26 -0500

    lms phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/student/Home'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={ <Home/>} />
+      </Routes>
+    </div>
   )
 }
 
