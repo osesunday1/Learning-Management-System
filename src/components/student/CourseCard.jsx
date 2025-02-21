d1ae02d611a18ce330cc69b4c2c025fb220db6dc
osesunday1 <ose.sunday@gmail.com>
2025-02-21 06:19:21 -0500

    pst

--- src/components/student/CourseCard.jsx
+++ src/components/student/CourseCard.jsx
@@ -13,7 +13,7 @@ const CourseCard = ({course}) => {
       <img className="w-full" src={course.courseThumbnail} alt="" />
       <div className="p-3 text-left">
         <h3 className="text-base font-semibold">{course.courseTitle}</h3>
-        <p className="text-gray-500">Ose Sunday</p>
+        <p className="text-gray-500">pst Funmi Olubakin</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
           <div className="flex">
